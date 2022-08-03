3daa0ee9405c457590922545aa6cf23bf08357b8
Keralauniversity-departmentsunion <110475743+Keralauniversity-departmentsunion@users.noreply.github.com>
2022-08-03 15:11:33 +0530

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -161,7 +161,7 @@ else:
 				lst=[name,mob,mail,gen,fac,str(d),event]
 				registration(lst)
 				
-with st.expander("Developers"):
+with st.sidebar.expander("Developers"):
  	st.markdown('#### [Prabin Raj K P](https://www.linkedin.com/in/prabinrajkp18/)')
  	st.markdown('#### [Vijay V Venkitesh](https://www.linkedin.com/in/vijay-v-venkitesh-673177204/)')
  	st.write('##### MSc Data Science \n Department of Futures Studies')
